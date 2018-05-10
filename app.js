689083e515afeba3278e72f9b8c452329908931f
JoSaGuDu <joseguti5@gmail.com>
2018-05-09 22:46:56 -0400

    data persistence added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("Yelp Camp app v1 to node control");
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const mongo = require("mongoose");
+const mongoose = require("mongoose");
 
 //Init express
 const app = express();
@@ -13,51 +13,128 @@ const app = express();
 app.use(express.static("public"));
 
 //Demand the use of body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //Set view engine
 app.set("view engine", "ejs");
 
+//Connect mongoose
+mongoose.connect('mongodb://localhost/yelp_camp');
+
+//Schema setup
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+//Compile the Schema into a model
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+
+//test adding camp
+/*Campground.create({
+      name: "Banff National Park",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Johnston_Canyon-Lower_Falls.jpg",
+      description: "Rocky Mountain peaks, turquoise glacial lakes, a picture-perfect mountain town and village, abundant wildlife and scenic drives come together in Banff National Park - Canada’s first national park and the flagship of the nation’s park system."
+}, function(error, campground){
+  if(error){
+    console.log(error);
+  } else {
+    console.log("New campground created!");
+    console.log(campground);
+  }
+});*/
+
 //Global variables
 
-//Temporary camps array 
+/*//Temporary camps array
 const campgrounds = [
         {name: "Banff National Park", image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Johnston_Canyon-Lower_Falls.jpg"},
         {name: "Glacier National Park", image: "https://upload.wikimedia.org/wikipedia/commons/7/7e/St_Mary_Lake.jpg"},
         {name: "Riding Mountain National Park", image: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Riding_Mountain_National_Park_Manitoba_Canada_%288%29.JPG"}
-];
+];*/
 /*------------------------
-          ROUTES           
+          ROUTES
 ---------------------------*/
+
 //root route
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/", function(req, res) {
+  res.redirect("/index");
 });
 
-//Campground routes
-app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds: campgrounds});
+//INDEX
+app.get("/index", function(req, res) {
+  //Get all the campgrounds from the temporary array
+  //res.render("index", {campgrounds: campgrounds});Replaced by the lookup to the database
+
+  //Get all the campgrounds from the DB
+  Campground.find({}, function(error, allCampgrounds) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.render("index", {
+        campgrounds: allCampgrounds
+      });
+    }
+  })
 })
-app.get("/campgrounds/new", function(req,res){
-  res.render("new.ejs");
-}),
-//post route
-app.post("/campgrounds", function(req, res){//Doesnt maters that the name of the route is the same because the method is diferent.
+
+//NEW
+app.get("/index/new", function (req, res) {
+  res.render("new");
+})
+
+//CREATE
+app.post("/index", function (req, res) { //Doesn't maters that the name of the route is the same because the method is diferent.
   //get data from form and add to the campgrouds array
   const campName = req.body.name;
   const campImgUrl = req.body.imgUrl;
-  const newCamp = {name: campName, image: campImgUrl};
-  campgrounds.push(newCamp);
-  //redirect back to campgrounds page
-  res.redirect("/campgrounds");
-})
+  const campDescription = req.body.description;
+  const newCamp = {
+    name: campName,
+    image: campImgUrl,
+    description: campDescription
+  };
+  console.log(newCamp);
+  //index.push(newCamp);Replaced by the Campground.create to the database
+  //
+  //Create a new campground and save it to the DB.
+  Campground.create(newCamp, function (error, newCampground) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("New campground created!");
+      console.log(newCampground);
+      //redirect back to campgrounds page
+      res.redirect("/index");
+    }
+  });
+});
+
+//SHOW (remember this route must be declared after any other route like this /root/anything)
+app.get("/index/:id", function (req, res) {
+  const campId = req.params.id;
+  Campground.findById(campId, function (error, foundCampground) {
+    console.log(foundCampground);
+    if (error) {
+      console.log(error);
+    } else {
+      res.render("show", {
+        campground: foundCampground
+      });
+    }
+  });
+});
 
 //Responding to all the rest of requests
-app.get("*", function(req, res){
-    res.send("Sorry, page not found! What are you doing with your life?");
+app.get("*", function (req, res) {
+  res.send("Sorry, page not found! What are you doing with your life?");
 });
 
 //Initializing the server: Specify the port of listening
-app.listen(8000, function(){
-    console.log("Engines on!");
+app.listen(8000, function () {
+  console.log("Engines on!");
 });
